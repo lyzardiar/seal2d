8e47578f04a4e9075c6c3874cbfa6b5b1c98f5ed
tangyiyang <sleepers.tang@gmail.com>
2016-07-05 22:25:55 +0800

    remove some unsed log

--- seal/render.c
+++ seal/render.c
@@ -15,29 +15,22 @@ struct vertex_buffer* vertex_buffer_new() {
     struct vertex_buffer* buffer = STRUCT_NEW(vertex_buffer);
     
     glGenVertexArrays(1, &buffer->vao);
-    CHECK_GL_ERROR;
     glBindVertexArray(buffer->vao);
-    CHECK_GL_ERROR;
     
     glGenBuffers(1, &buffer->vbo);
-    CHECK_GL_ERROR;
     glBindBuffer(GL_ARRAY_BUFFER, buffer->vbo);
-    CHECK_GL_ERROR;
     
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
     glEnableVertexAttribArray(2);
-    CHECK_GL_ERROR;
     
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, VERTEX_SIZE, VERTEX_OFFSET_POS);
     glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_TRUE, VERTEX_SIZE, VERTEX_OFFSET_COLOR);
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, VERTEX_SIZE, VERTEX_OFFSET_UV);
-    CHECK_GL_ERROR;
     
     glDisableVertexAttribArray(0);
     glDisableVertexAttribArray(1);
     glDisableVertexAttribArray(2);
-    CHECK_GL_ERROR;
     
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -62,13 +55,6 @@ void vertex_buffer_add(struct vertex_buffer* self, struct glyph* glyph) {
     buf[3] = glyph->bl;
     buf[4] = glyph->br;
     buf[5] = glyph->tr;
-    
-    for (int i = 0; i < 6; ++i) {
-        struct vertex data = buf[i];
-        printf("buff {%.2f, %.2f}, {%d, %d, %d, %d}, {%.2f, %.2f}\n", data.position[0], data.position[1],
-               data.color[0], data.color[1], data.color[2], data.color[3],
-               data.uv[0], data.uv[1]);
-    }
 }
 
 struct render* render_new() {
@@ -93,25 +79,16 @@ void render_free(struct render* self) {
 }
 
 void render_clear(struct render* self, unsigned long color) {
-    
     float r = ((color >> 24) & 0xff) / 255.0;
     float g = ((color >> 16) & 0xff) / 255.0;
     float b = ((color >> 8 ) & 0xff) / 255.0;
     float a = ((color      ) & 0xff) / 255.0;
     
-    
     glClearDepth(1.0f);
-    CHECK_GL_ERROR;
     glClearColor(r, g, b, a);
-    CHECK_GL_ERROR;
-    
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    CHECK_GL_ERROR;
-    
     glEnable(GL_BLEND);
-    CHECK_GL_ERROR;
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    CHECK_GL_ERROR;
     
     self->drawcall = 0;
 }
@@ -119,45 +96,30 @@ void render_clear(struct render* self, unsigned long color) {
 void render_commit(struct render* self) {
     if (self->render_state & RENDER_STATE_PROGRAM_BIT) {
         glUseProgram(self->cur_program);
-        CHECK_GL_ERROR;
-        printf("1 commit program.\n");
     }
 
     struct vertex_buffer* vbuf = self->vertex_buffer;
     if (self->render_state & RENDER_STATE_VERTEX_BIT) {
         glBindBuffer(GL_ARRAY_BUFFER, vbuf->vbo);
-        CHECK_GL_ERROR;
         glBufferData(GL_ARRAY_BUFFER, VERTEX_SIZE*vbuf->n_objs * 6, vbuf->data, GL_DYNAMIC_DRAW);
-        CHECK_GL_ERROR;
         glBindBuffer(GL_ARRAY_BUFFER, 0);
-        CHECK_GL_ERROR;
         
         glBindVertexArray(vbuf->vao);
-        CHECK_GL_ERROR;
-        printf("2 commit vertex buffer.\n");
     }
     
     // TODO: ungly hacking here.
     if (self->render_state & RENDER_STATE_TEXTURE_BIT) {
         glActiveTexture(GL_TEXTURE0);
-        CHECK_GL_ERROR;
         glBindTexture(GL_TEXTURE_2D, self->cur_tex_id);
-        CHECK_GL_ERROR;
         GLint texture_location = glGetUniformLocation(self->cur_program, "sampler");
-        CHECK_GL_ERROR;
         glUniform1i(texture_location, 0);
-        CHECK_GL_ERROR;
-        printf("3 commit texture.\n");
     }
     
     if (self->render_state) {
         GLint projection = glGetUniformLocation(self->cur_program, "projection");
-        CHECK_GL_ERROR;
         glUniformMatrix4fv(projection, 1, GL_FALSE, GAME->global_camera->camer_mat->m);
-        CHECK_GL_ERROR;
         
         glBindBuffer(GL_ARRAY_BUFFER, vbuf->vbo);
-        CHECK_GL_ERROR;
         
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, VERTEX_SIZE, VERTEX_OFFSET_POS);
@@ -169,17 +131,9 @@ void render_commit(struct render* self) {
         glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, VERTEX_SIZE, VERTEX_OFFSET_UV);
         
         glDrawArrays(GL_TRIANGLES, 0, vbuf->n_objs * 6);
-        CHECK_GL_ERROR;
-
-        printf("4 commit dracall.\n");
         self->drawcall++;
     }
-    
-//    if (self->render_state & RENDER_STATE_VERTEX_BIT) {
-//        glBindVertexArray(0);
-//        CHECK_GL_ERROR;
-//    }
-    
+
     render_clear_state(self);
 }
 
